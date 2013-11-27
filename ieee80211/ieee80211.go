f54696f392c2a7a8050df9fe64ed94ecff4ad305
Rob Figueiredo <robfig@gmail.com>
2013-11-27 00:53:57 -0500

    further improve ieee80211 parsing

--- ieee80211/ieee80211.go
+++ ieee80211/ieee80211.go
@@ -4,73 +4,162 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+
 	"net"
 )
 
-// Header fields are present in all Management and Data frames.
-type Header struct {
-	FrameControl    uint16
-	DurationID      uint16
-	Addr1           [6]byte
-	Addr2           [6]byte
-	Addr3           [6]byte
-	SequenceControl uint16
+const (
+	// FrameControl bits
+	FC_Version  = 0x0003
+	FC_Type     = 0x000C
+	FC_Subtype  = 0x00F0
+	FC_ToDS     = 0x0100
+	FC_FromDS   = 0x0200
+	FC_MoreFrag = 0x0400
+	FC_Retry    = 0x0800
+	FC_PwrMgmt  = 0x1000
+	FC_MoreData = 0x2000
+	FC_WEB      = 0x4000
+	FC_Rsvd     = 0x8000
+
+	// Frame types
+	Type_Management = 0x0
+	Type_Control    = 0x1
+	Type_Data       = 0x2
+
+	// Management frames
+	// Addr1, Addr2, Addr3, Sequence Control are present in all subtypes.
+	Subtype_AssociationRequest    = 0x0
+	Subtype_AssociationResponse   = 0x1
+	Subtype_ReassociationRequest  = 0x2
+	Subtype_ReassociationResponse = 0x3
+	Subtype_ProbeRequest          = 0x4
+	Subtype_ProbeResponse         = 0x5
+	Subtype_TimingAdvertisement   = 0x6
+	Subtype_Beacon                = 0x8
+	Subtype_ATIM                  = 0x9
+	Subtype_Disassociation        = 0xA
+	Subtype_Authentication        = 0xB
+	Subtype_Deauthentication      = 0xC
+	Subtype_Action                = 0xD
+	Subtype_ActionNoAck           = 0xE
+
+	// Control frames
+	// Addr1 is present in all subtypes.
+	Subtype_ControlWrapper  = 0x7 // Addr1
+	Subtype_BlockAckRequest = 0x8 // RA, TA
+	Subtype_BlockAck        = 0x9 // RA, TA
+	Subtype_PSPoll          = 0xA // RA (BSSID), TA
+	Subtype_RTS             = 0xB // RA, TA
+	Subtype_CTS             = 0xC // RA
+	Subtype_ACK             = 0xD // RA
+	Subtype_CFEnd           = 0xE // RA, TA (BSSID)
+	Subtype_CFEndAck        = 0xF // RA, TA (BSSID)
+
+	// Data frames
+	// Addr1, Addr2, Addr3, Sequence Control are present in all subtypes.
+	Subtype_Data             = 0x0
+	Subtype_DataCFAck        = 0x1
+	Subtype_DataCFPoll       = 0x2
+	Subtype_DataCFAckPoll    = 0x3
+	Subtype_Null             = 0x4
+	Subtype_CFAck            = 0x5
+	Subtype_CFPoll           = 0x6
+	Subtype_CFAckPoll        = 0x7
+	Subtype_QosData          = 0x8
+	Subtype_QosDataCFAck     = 0x9
+	Subtype_QosDataCFPoll    = 0xA
+	Subtype_QosDataCFAckPoll = 0xB
+	Subtype_QosNull          = 0xC
+	Subtype_QosCFPoll        = 0xE
+	Subtype_QosCFAckPoll     = 0xF
+)
+
+type FrameControl uint16
+
+func (fc FrameControl) Type() uint8 {
+	return uint8(fc & FC_Type >> 2)
+}
+
+func (fc FrameControl) Subtype() uint8 {
+	return uint8(fc & FC_Subtype >> 4)
 }
 
 type Frame struct {
-	Header
-	Addr4 [6]byte // Present when ToDS and FromDS
-	QoS   uint16  // Present in some Subtypes
-	Body  []byte  // Payload
-	FCS   uint32  // CRC32
+	FrameControl    FrameControl
+	DurationID      uint16
+	Addr1           net.HardwareAddr
+	Addr2           net.HardwareAddr
+	Addr3           net.HardwareAddr
+	SequenceControl uint16
+	Addr4           net.HardwareAddr // Present when ToDS and FromDS
+	QoS             uint16
+	Body            []byte // Payload
+	FCS             uint32 // CRC32
 }
 
-// 16 FrameControl bits
-const (
-	Version  = 0x0003
-	Type     = 0x000C
-	Subtype  = 0x00F0
-	ToDS     = 0x0100
-	FromDS   = 0x0200
-	MoreFrag = 0x0400
-	Retry    = 0x0800
-	PwrMgmt  = 0x1000
-	MoreData = 0x2000
-	WEB      = 0x4000
-	Rsvd     = 0x8000
-)
+// Control ACK is the smallest legitimate packet:
+// FrameControl, DurationID, Addr1, FCS
+const MinPacketSize = 14
 
-const ReqdHeaderSize = 24
-
-func Parse(packet []byte) (*Frame, error) {
-	if len(packet) < ReqdHeaderSize+4 {
+func Parse(packet []byte) (pframe *Frame, err error) {
+	if len(packet) < MinPacketSize {
 		return nil, errors.New("invalid frame: too short")
 	}
 
 	var frame Frame
-	err := binary.Read(bytes.NewReader(packet[:ReqdHeaderSize]), binary.LittleEndian, &frame.Header)
-	if err != nil {
-		return nil, err
+	if !read(&err, packet[:2], &frame.FrameControl) ||
+		!read(&err, packet[2:4], &frame.DurationID) {
+		return
 	}
 
-	// Addr4 is present if ToDS and FromDS
-	var headerSize = ReqdHeaderSize
-	if frame.FrameControl&(ToDS|FromDS) == (ToDS | FromDS) {
-		headerSize += 6
-		err = binary.Read(bytes.NewReader(packet[ReqdHeaderSize:headerSize]),
-			binary.LittleEndian, &frame.Addr4)
-		if err != nil {
-			return nil, err
+	var pos = 4
+	switch frame.FrameControl.Type() {
+	case Type_Management, Type_Data:
+		// Addr1, Addr2, Addr3, Sequence Control present in all management/data frames.
+		frame.Addr1 = net.HardwareAddr(packet[4:10])
+		frame.Addr2 = net.HardwareAddr(packet[10:16])
+		frame.Addr3 = net.HardwareAddr(packet[16:22])
+		if !read(&err, packet[22:24], &frame.SequenceControl) {
+			return
+		}
+		pos = 24
+
+	case Type_Control:
+		// (ControlWrapper, CTS, ACK) have only Addr1.  Others have Addr1, Addr2.
+		switch frame.FrameControl.Subtype() {
+		case Subtype_ControlWrapper, Subtype_CTS, Subtype_ACK:
+			frame.Addr1 = net.HardwareAddr(packet[4:10])
+			pos = 10
+		default:
+			frame.Addr1 = net.HardwareAddr(packet[4:10])
+			frame.Addr2 = net.HardwareAddr(packet[10:16])
+			pos = 16
 		}
 	}
 
-	// TODO: 2 byte QoS may be present in certain subtypes.
+	// Addr4 is present if ToDS and FromDS
+	if frame.FrameControl&(FC_ToDS|FC_FromDS) == (FC_ToDS | FC_FromDS) {
+		frame.Addr4 = packet[pos : pos+6]
+		pos += 6
+	}
+
+	// QoS present for data packets, subtype >= QosData
+	if frame.FrameControl&FC_Type == Type_Data && frame.FrameControl&FC_Subtype >= Subtype_QosData {
+		if !read(&err, packet[pos:pos+2], &frame.QoS) {
+			return
+		}
+		pos += 2
+	}
 
 	var fcsStart = len(packet) - 4
-	frame.Body = packet[headerSize:fcsStart]
-	err = binary.Read(bytes.NewReader(packet[fcsStart:]), binary.LittleEndian, &frame.FCS)
-	if err != nil {
-		return nil, err
+	if fcsStart < pos {
+		// TODO: This can probably happen if we don't capture the entire packet.
+		return nil, errors.New("FCS not found")
+	}
+	frame.Body = packet[pos:fcsStart]
+	if !read(&err, packet[fcsStart:], &frame.FCS) {
+		return
 	}
 
 	return &frame, nil
@@ -78,56 +167,26 @@ func Parse(packet []byte) (*Frame, error) {
 
 func (h *Frame) Source() net.HardwareAddr {
 	var (
-		toDS   = h.FrameControl&ToDS != 0
-		fromDS = h.FrameControl&FromDS != 0
+		toDS   = h.FrameControl&FC_ToDS != 0
+		fromDS = h.FrameControl&FC_FromDS != 0
 	)
 	if !fromDS {
-		return net.HardwareAddr(h.Addr2[:])
+		return h.Addr2
 	}
 	if !toDS {
-		return net.HardwareAddr(h.Addr3[:])
+		return h.Addr3
 	}
-	return net.HardwareAddr(h.Addr4[:])
+	return h.Addr4
 }
 
-func (h *Header) Dest() net.HardwareAddr {
-	if h.FrameControl&ToDS == 0 {
-		return net.HardwareAddr(h.Addr1[:])
+func (h *Frame) Dest() net.HardwareAddr {
+	if h.FrameControl&FC_ToDS == 0 {
+		return h.Addr1
 	}
-	return net.HardwareAddr(h.Addr3[:])
+	return h.Addr3
 }
 
-func (h *Header) FrameType() FrameType {
-	return FrameType((h.FrameControl&Type)<<2 + h.FrameControl&Subtype>>4)
+func read(err *error, in []byte, out interface{}) bool {
+	*err = binary.Read(bytes.NewReader(in), binary.LittleEndian, out)
+	return *err == nil
 }
-
-const (
-	Management = 0
-	Control    = 1
-	Data       = 2
-)
-
-type FrameType uint8
-
-const (
-	// Management
-	AssociationRequest    FrameType = 0x00
-	AssociationResponse             = 0x01
-	ReassociationRequest            = 0x02
-	ReassociationResponse           = 0x03
-	ProbeRequest                    = 0x04
-	ProbeResponse                   = 0x05
-	Beacon                          = 0x08
-	ATIM                            = 0x09
-	Disassociation                  = 0x0A
-	Authentication                  = 0x0B
-	Deauthentication                = 0x0C
-
-	// Control
-	PowerSave         FrameType = 0x1A
-	RequestToSend               = 0x1B
-	ClearToSend                 = 0x1C
-	Ack                         = 0x1D
-	ContentionFree              = 0x1E
-	ContentionFreeAck           = 0x1F
-)
--- ieee80211/ieee80211_test.go
+++ ieee80211/ieee80211_test.go
@@ -7,24 +7,17 @@ import (
 
 // TODO: Find packets with QoS / Addr4
 
-func TestIEEE80211(t *testing.T) {
+func TestIEEE80211_Ack(t *testing.T) {
 	var b = []byte{
-		0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x28, 0xcf, 0xda, 0xb2,
-		0x16, 0xd0, 0x28, 0xcf, 0xda, 0xb2, 0x16, 0xd0, 0xf0, 0x90,
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, // dummy body bytes
-		0x8c, 0x39, 0x8d, 0x11,
+		0xd4, 0, 0, 0, 0xb8, 0xe8, 0x56, 0x2d, 0xb9, 0x36, 0xe6, 0x5f, 0x03, 0x7f,
 	}
+
 	var expected = &Frame{
-		Header: Header{
-			FrameControl:    0x0080,
-			DurationID:      0,
-			Addr1:           [6]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
-			Addr2:           [6]byte{0x28, 0xcf, 0xda, 0xb2, 0x16, 0xd0},
-			Addr3:           [6]byte{0x28, 0xcf, 0xda, 0xb2, 0x16, 0xd0},
-			SequenceControl: 0x90f0,
-		},
-		Body: []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-		FCS:  0x8c398d11,
+		FrameControl: 0x00d4,
+		DurationID:   0,
+		Addr1:        []byte{0xb8, 0xe8, 0x56, 0x2d, 0xb9, 0x36},
+		Body:         []byte{},
+		FCS:          0x7f035fe6,
 	}
 
 	actual, err := Parse(b)
@@ -33,12 +26,56 @@ func TestIEEE80211(t *testing.T) {
 		return
 	}
 
-	if !reflect.DeepEqual(actual.Header, expected.Header) {
-		t.Errorf("Bad header:\n%v (expected)\n%v (actual)", expected.Header, actual.Header)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Bad header:\n%v (expected)\n%v (actual)", expected, actual)
+	}
+	if actual.FrameControl.Type() != Type_Control {
+		t.Errorf("Expected type control, got %b", actual.FrameControl.Type())
+	}
+	if actual.FrameControl.Subtype() != Subtype_ACK {
+		t.Errorf("Expected ack, got %b", actual.FrameControl.Subtype())
+	}
+	if actual.Dest().String() != "b8:e8:56:2d:b9:36" {
+		t.Error("Unexpected dest addr:", actual.Dest())
+	}
+	if len(actual.Body) != 0 {
+		t.Error("Incorrect body, len", len(actual.Body))
+	}
+}
+
+func TestIEEE80211_Beacon(t *testing.T) {
+	var b = []byte{
+		0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x28, 0xcf, 0xda, 0xb2,
+		0x16, 0xd0, 0x28, 0xcf, 0xda, 0xb2, 0x16, 0xd0, 0xf0, 0x90,
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, // dummy body bytes
+		0x8c, 0x39, 0x8d, 0x11,
+	}
+	var expected = &Frame{
+		FrameControl:    0x0080,
+		DurationID:      0,
+		Addr1:           []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+		Addr2:           []byte{0x28, 0xcf, 0xda, 0xb2, 0x16, 0xd0},
+		Addr3:           []byte{0x28, 0xcf, 0xda, 0xb2, 0x16, 0xd0},
+		SequenceControl: 0x90f0,
+		Body:            []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		FCS:             0x118d398c,
 	}
 
-	if actual.FrameType() != Beacon {
-		t.Errorf("Expected beacon, got %b", actual.FrameType())
+	actual, err := Parse(b)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Bad header:\n%v (expected)\n%v (actual)", expected, actual)
+	}
+
+	if actual.FrameControl.Type() != Type_Management {
+		t.Errorf("Expected type management, got %b", actual.FrameControl.Type())
+	}
+	if actual.FrameControl.Subtype() != Subtype_Beacon {
+		t.Errorf("Expected beacon, got %b", actual.FrameControl.Subtype())
 	}
 
 	if actual.Source().String() != "28:cf:da:b2:16:d0" {
